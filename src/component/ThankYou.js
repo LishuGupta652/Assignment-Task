7ece87022b7df941def2a5bd0cefa480be05ef94
lishu <lishugupta652@gmail.com>
2021-12-30 21:04:42 +0530

    Post data

--- src/component/ThankYou.js
+++ src/component/ThankYou.js
@@ -2,14 +2,30 @@ import React, { useEffect, useState } from "react";
 import { useGlobalSetState, useGlobalState } from "../context/globalContext";
 import { Container } from "../styles/GlobalStyles";
 import { HomeContainer } from "../styles/HomeStyles";
-
+import axios from "axios";
 const ThankYou = () => {
   const data = useGlobalState();
   const setData = useGlobalSetState();
 
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
+    axios
+      .post("http://localhost:3000/api/techwondoe", data)
+      .then((response) => {
+        setLoading(false);
+        console.log(response);
+        setData({
+          name: "",
+          email: "",
+          phoneNumber: "",
+          password: "",
+          checkPassword: "",
+          declaration: "",
+        });
+      });
+
     console.log(data);
   });
   return (
@@ -19,6 +35,9 @@ const ThankYou = () => {
           Thank you for your resonse{" "}
           <span className="user_name">{data.name} </span>
         </h1>
+        <Link to="/">
+          <button>Restart</button>
+        </Link>
       </Container>
     </HomeContainer>
   );
